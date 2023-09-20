8a2727be5532cdb2176ccd9a6fb1b2d30619ce0a
Dmitriy Zuborev <Tyuiu.ZuborevDA@outlook.com>
2023-09-20 19:28:17 +0500

    Task1

---
+++ Tyuiu.ZuborevDA.Sprint1.Task1.V28.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ZuborevDA.Sprint1.Task1.V28.Lib;
+
+namespace Tyuiu.ZuborevDA.Sprint1.Task1.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            var res = ds.Calculate(x);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
---
+++ Tyuiu.ZuborevDA.Sprint1.Task1.V28/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ZuborevDA.Sprint1.Task1.V28.Lib;
+
+namespace Tyuiu.ZuborevDA.Sprint1.Task1.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Зуборев Д. А. | АСОиУБ-23-2";
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                  *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                           *");
+            Console.WriteLine("* Задание #1                                                                 *");
+            Console.WriteLine("* Вариант #28                                                                *");
+            Console.WriteLine("* Выполнил: Зуборев Дмитрий Александрович | АСОиУБ-23-2                      *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                   *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,    *");
+            Console.WriteLine("* вычисляет результат по формуле (x*2)/(1+x) и печатает его на экране.       *");
+            Console.WriteLine("*                                                                            *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                           *");
+            Console.WriteLine("******************************************************************************");
+
+            double x;
+
+            Console.WriteLine("Ввидите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+            Console.WriteLine("******************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x));
+
+            Console.ReadLine();
+        }
+    }
+}
