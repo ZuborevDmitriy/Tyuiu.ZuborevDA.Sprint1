fe5e451a07e8cdab4c62bae703cb1a743c0b4a55
Dmitriy Zuborev <Tyuiu.ZuborevDA@outlook.com>
2023-09-26 20:11:54 +0500

    Task4

---
+++ Tyuiu.ZuborevDA.Sprint1.Task4.V1.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ZuborevDA.Sprint1.Task4.V1.Lib;
+
+namespace Tyuiu.ZuborevDA.Sprint1.Task4.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double wait = 1.0 / 9;
+            double result = Math.Round(wait, 3);
+            var res = ds.Calculate(x);
+            Assert.AreEqual(result, res);
+        }
+    }
+}
